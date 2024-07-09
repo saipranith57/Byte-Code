b5d1079f916c622d69f898be7bfb6eb5dac0445b
saipranith57 <saipranith57@gmail.com>
2024-07-09 15:01:52 +0530

    commit

---
+++ marks_of_Robert.java
@@ -0,0 +1,13 @@
+public class marks_of_Robert {
+
+    public static void main(String[] args) {
+        byte marks1 = 78, marks2 = 48, marks3 = 62;
+        int total_marks = 300;
+        int  result = (marks1+marks2+marks3);
+        double percent;
+        percent = (double)result/total_marks*100;
+        System.out.println(result);
+        System.out.println(percent);
+
+    }
+}
