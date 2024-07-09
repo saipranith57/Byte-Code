afe767090ee531eb44cc8dc0a696619aae9f3605
saipranith57 <saipranith57@gmail.com>
2024-07-09 16:26:14 +0530

    commit

---
+++ add_8_to_the_number_2345.java
@@ -0,0 +1,11 @@
+public class add_8_to_the_number_2345 {
+    public static void main(String[] args) {
+        long num1 = 8;
+        long num2 =2345;
+        num2+=num1;
+        num2/=3;
+        num2%=5;
+        num2*=5;
+        System.out.println("Final Result "+num2);
+    }
+}
